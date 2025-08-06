126e6371a3e67ec095f7e1a2a2ee37a7eec68c24
Maria Clara <guedesoliveira004@gmail.com>
2025-08-05 22:55:36 -0300

    feat: inserting the route of forget-password

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 
 export const routes: Routes = [
-    { 
+  { 
     path: 'home',
     component: HomeComponent,
     title: 'Página Inicial' // Título da página (opcional)
+  },
+  { 
+    path: 'forget-password',
+    component: ForgotPasswordComponent,
+    title: 'Recuperar senha' // Título da página (opcional)
   },
 ];
