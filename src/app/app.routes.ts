99dda8b0fd965452e7e52cac09ea3f811e9152d1
Filipe Luiz <64997111+FLuiz22@users.noreply.github.com>
2025-08-10 06:50:52 -0300

    Revert "Tecendo arte 3/login screen"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,3 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [
-    {
-    path: 'home',
-    component: HomeComponent,
-    title: 'Página Inicial' // Título da página (opcional)
-  },
-  {
-    path: 'login',
-    loadComponent: ()=>
-      import('./pages/login/login.component').then(m => m.LoginComponent)
-  }
-];
+export const routes: Routes = [];
