2e2468b9e18a41b936ee443271fcb0ecc789f391
devLarissaRodrigues <larissa.rodrigues@ccc.ufcg.edu.br>
2025-08-15 20:37:43 -0300

    Fix - changing some styling aspects to get the result closer to figma project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     {
@@ -8,7 +9,6 @@ export const routes: Routes = [
   },
   {
     path: 'login',
-    loadComponent: ()=>
-      import('./pages/login/login.component').then(m => m.LoginComponent)
+    component: LoginComponent,
   }
 ];
