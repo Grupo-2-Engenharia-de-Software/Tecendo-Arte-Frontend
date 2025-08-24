f386cda892bca7483f16c034018aa249a83e8112
FLuiz22 <filipeluiz39@gmail.com>
2025-08-23 21:01:08 -0300

    Feat: usuario service added

---
+++ src/app/services/usuario/usuario.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+  private backend = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  criarConta(conta: any): Observable<any> {
+    return this.http.post(`${this.backend}/api/conta`, conta);
+  } 
+
+}
