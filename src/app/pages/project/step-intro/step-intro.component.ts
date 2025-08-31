6264c65053e4e037be0e7e7d3765515e5d389d15
Maria Clara <guedesoliveira004@gmail.com>
2025-08-31 12:10:40 -0300

    feat: page of project info

--- src/app/pages/project/step-intro/step-intro.component.ts
+++ src/app/pages/project/step-intro/step-intro.component.ts
@@ -1,20 +1,29 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { RouterModule, Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-step-intro',
-  standalone: true,
-  imports: [FormsModule, RouterModule],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './step-intro.component.html',
+  styleUrl: './step-intro.component.scss'
 })
 export class StepIntroComponent {
   name = '';
+  form: FormGroup;
 
-  constructor(private router: Router) {}
-
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.form = this.fb.group({
+      nome: ['', Validators.required],
+      periodo: ['', Validators.required],
+      descricao: ['', Validators.required],
+      meta: ['', Validators.required]
+    });
+  }
+  
   next() {
-    // Poderia emitir evento para atualizar estado do pai
+    sessionStorage.setItem('initial-info', JSON.stringify(this.form.value));
     this.router.navigate(['project/reward']);
   }
+
 }
