1fda4dad31eadebf3a2463342473c075508c8745
Maria Clara <guedesoliveira004@gmail.com>
2025-09-07 18:21:55 -0300

    fix: toast message

--- src/app/guards/tipo-conta.guard.ts
+++ src/app/guards/tipo-conta.guard.ts
@@ -1,10 +1,12 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { UsuarioService } from '../services/usuario/usuario.service';
 import { inject } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 export const tipoContaGuard: CanActivateFn = (route, state) => {
   const usuarioService = inject(UsuarioService);
   const router = inject(Router);
+  const toastr = inject(ToastrService);
 
   const usuario = usuarioService.getLoggedUser();
 
@@ -13,5 +15,9 @@ export const tipoContaGuard: CanActivateFn = (route, state) => {
   }
 
   router.navigate(['/']);
+  toastr.warning('É necessário ser artista para acessar esta página.', '', {
+    timeOut: 2000,
+    positionClass: 'toast-top-center',
+  });
   return false;
 };
